2847ab88945ccb444a89bf9cee8a32a90c58467d
CodeOfPanda <#Thailand052218>
2021-10-16 13:12:52 -0600

    small updates

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def load_package_data():
     with open('./data/Package_File.csv', 'r') as package_file:
         file = package_file.readlines()
         data_array = [row.split(',') for row in file]
-        data_array.pop(0)  # removing the first element from the array (column headers)
+        # removing the first element from the array (column headers)
+        data_array.pop(0)
         h = HashTable()
-        
+
         for data in data_array:
             p_id = int(data[0])
             address = data[1]
@@ -21,8 +22,9 @@ def load_package_data():
             mass_kg = int(data[6])
             notes = data[7]
             status = 'AT_HUB'
-            
-            pkg = Package(p_id, address, city, state, zip, delivery_deadline, mass_kg, notes, status)
+
+            pkg = Package(p_id, address, city, state, zip,
+                          delivery_deadline, mass_kg, notes, status)
             h.insert(p_id, pkg)
     return h
 
@@ -31,40 +33,37 @@ def load_package_data():
 # print("--------------------------------")
 # print(pkg_hash_map.print())
 
+
 def load_distance_data():
     # loading header data to created vertices
     with open('./data/WGUPS_Distance_Table.csv', 'r', encoding='utf-8-sig') as dist_header_data:
         header_file = dist_header_data.readlines()
-        headers = [row.split(',\n') for row in header_file[:28]]
-        vertices = [] 
+        headers = [row.split(',\n') for row in header_file[:27]]
+        vertices = []
         graph = Graph()
-        
+
         for line in headers:
             vertex = Vertex(line[0])
             vertices.append(vertex)
             graph.add_vertex(vertex)
-            
-            
+
     with open('./data/WGUPS_Distance_Table.csv', 'r', encoding='utf-8-sig') as dist_row_data:
         row_file = dist_row_data.readlines()
         rows = [line.split(',\n') for line in row_file[27:]]
-        
+
         for i, row in enumerate(rows):
             miles = row[0].split(',')
             vertex_1 = vertices[i]
+
             for j, m in enumerate(miles):
-                if not m:
+                if m:
+                    vertex_2 = vertices[j]
+                    # Removing the data for vertex to the same vertex, basically just cleans the data
+                    if vertex_1.get_label() != vertex_2.get_label():
+                        graph.add_undirected_edge(vertex_1, vertex_2, m)
+                else:
                     break
-                vertex_2 = vertices[j]
-                graph.add_undirected_edge(vertex_1, vertex_2, m)
-                  
-        for k,v in graph.adjacency_list.items():
-            print(k.get_label())
-            print("--")
-            for vertex in v:
-                print(vertex.get_label())
-            print("-----------")
-            
+
     return vertices
 
 
