856ab33622f73de025bc3193f3cdd203245f5883
CodeOfPanda <#Thailand052218>
2021-10-17 14:04:08 -0600

    separated truck data into load_data.py

--- load_data.py
+++ load_data.py
@@ -2,17 +2,18 @@ from models.package import Package
 from models.hash_table import HashTable
 from models.vertex import Vertex
 from models.graph import Graph
+from models.truck import Truck
 
 
 def load_package_data():
-    # loading package data 
+    # loading package data
     with open('./data/Package_File.csv', 'r') as package_file:
         file = package_file.readlines()
         data_array = [row.split(',') for row in file]
         # removing the first element from the array (column headers)
         data_array.pop(0)
         h = HashTable()
-        
+
         # creating a new package object from each row of data in the data_array
         for data in data_array:
             p_id = int(data[0])
@@ -24,9 +25,11 @@ def load_package_data():
             mass_kg = int(data[6])
             notes = data[7]
             status = 'AT_HUB'
-            
-            pkg = Package(p_id, address, city, state, zip, delivery_deadline, mass_kg, notes, status)
-            h.insert(p_id, pkg)  # inserting the key/value pair into the hash_table
+
+            pkg = Package(p_id, address, city, state, zip,
+                          delivery_deadline, mass_kg, notes, status)
+            # inserting the key/value pair into the hash_table
+            h.insert(p_id, pkg)
     return h
 
 
@@ -34,17 +37,19 @@ def load_distance_data():
     # loading distance data
     with open('./data/WGUPS_Distance_Table.csv', 'r', encoding='utf-8-sig') as dist_data:
         dist_file = dist_data.readlines()
-        headers = [row.split(',\n') for row in dist_file[:27]]  # taking the header addresses and placing them into an array
-        rows = [row.split(',\n') for row in dist_file[27:]]  # taking only the distance data (miles)
-        vertices = [] 
+        # taking the header addresses and placing them into an array
+        headers = [row.split(',\n') for row in dist_file[:27]]
+        # taking only the distance data (miles)
+        rows = [row.split(',\n') for row in dist_file[27:]]
+        vertices = []
         graph = Graph()
-        
+
         # creating vertex objects and adding them to an array and the graph
         for line in headers:
             vertex = Vertex(line[0])
             vertices.append(vertex)
             graph.add_vertex(vertex)
-                    
+
         # pairing the correct vertices with the distance between them and adding them to the graph.
         for i, row in enumerate(rows):
             miles = row[0].split(',')
@@ -61,3 +66,31 @@ def load_distance_data():
 
     return graph
 
+
+# Truck Data
+# Create three trucks
+t1 = Truck()
+t2 = Truck()
+t3 = Truck()
+# Package ids for each Truck
+t1_packages = [6, 25, 26, 31, 32, 28, 34, 39, 35, 33, 27, 24, 23, 22, 17, 12]
+t2_packages = [3, 18, 36, 38, 13, 14, 15, 16, 19, 20, 21, 1, 37, 29, 30, 40]
+t3_packages = [5, 9, 2, 4, 7, 8, 10, 11]
+
+
+def load_all_trucks(hashmap):
+    trucks = []
+    truck1 = load_truck(t1, t1_packages, hashmap)
+    trucks.append(truck1)
+    truck2 = load_truck(t2, t2_packages, hashmap)
+    trucks.append(truck2)
+    truck3 = load_truck(t3, t3_packages, hashmap)
+    trucks.append(truck3)
+    return trucks
+
+
+def load_truck(truck, truck_packages, pkg_hashmap):
+    for p in truck_packages:
+        package = pkg_hashmap.look_up(p)
+        truck.load_packages(package)
+    return truck
--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-from models.truck import Truck
-from load_data import load_package_data, load_distance_data
-
-t1 = Truck()
-t2 = Truck()
-t3 = Truck()
-t1_packages = [6, 25, 26, 31, 32, 28, 34, 39, 35, 33, 27, 24, 23, 22, 17, 12]
-t2_packages = [3, 18, 36, 38, 13, 14, 15, 16, 19, 20, 21, 1, 37, 29, 30, 40]
-t3_packages = [5, 9, 2, 4, 7, 8, 10, 11]
-
+from load_data import load_package_data, load_distance_data, load_all_trucks
 
 pkg_hashmap = load_package_data()
 graph = load_distance_data()
-
-def load_truck(truck, truck_packages):
-    for p in truck_packages:
-        package = pkg_hashmap.look_up(p)
-        truck.load_packages(package)
-        
-
-load_truck(t1, t1_packages)
-load_truck(t2, t2_packages)
-load_truck(t3, t3_packages)
+t1, t2, t3 = load_all_trucks(pkg_hashmap)
