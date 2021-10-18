2ba5d57b86df1351f114672d67ebb4074853fa55
CodeOfPanda <mlynnleigland@gmail.com>
2021-10-18 13:05:07 -0600

    loaded location data for each vertex

--- load_data.py
+++ load_data.py
@@ -34,22 +34,29 @@ def load_package_data():
 
 
 def load_distance_data():
+    vertices = []
+    graph = Graph()
+
+    #load delivery locations and addresses
+    with open('./data/Delivery_Locations.csv', 'r') as location_data:
+        loc_file = location_data.readlines()
+        headers = [row.split(',\n') for row in loc_file]
+        header_data = [loc[0].split(', ') for loc in headers]
+        
+        for data in header_data:
+            name = data[0]
+            address = data[1]
+            # creating vertex objects and adding them to an array and the graph
+            vertex = Vertex(name=name, address=address)
+            vertices.append(vertex)
+            graph.add_vertex(vertex)
+                
     # loading distance data
     with open('./data/WGUPS_Distance_Table.csv', 'r', encoding='utf-8-sig') as dist_data:
         dist_file = dist_data.readlines()
-        # taking the header addresses and placing them into an array
-        headers = [row.split(',\n') for row in dist_file[:27]]
         # taking only the distance data (miles)
         rows = [row.split(',\n') for row in dist_file[27:]]
-        vertices = []
-        graph = Graph()
-
-        # creating vertex objects and adding them to an array and the graph
-        for line in headers:
-            vertex = Vertex(line[0])
-            vertices.append(vertex)
-            graph.add_vertex(vertex)
-
+        
         # pairing the correct vertices with the distance between them and adding them to the graph.
         for i, row in enumerate(rows):
             miles = row[0].split(',')
@@ -59,26 +66,24 @@ def load_distance_data():
                 if m:
                     vertex_2 = vertices[j]
                     # Removing the data for vertex to the same vertex, basically just cleans the data
-                    if vertex_1.get_label() != vertex_2.get_label():
+                    if vertex_1.get_name() != vertex_2.get_name():
                         graph.add_undirected_edge(vertex_1, vertex_2, m)
                 else:
                     break
 
-    return graph
+    return [graph, vertices]
 
 
-# Truck Data
-# Create three trucks
-t1 = Truck()
-t2 = Truck()
-t3 = Truck()
-# Package ids for each Truck
-t1_packages = [6, 25, 26, 31, 32, 28, 34, 39, 35, 33, 27, 24, 23, 22, 17, 12]
-t2_packages = [3, 18, 36, 38, 13, 14, 15, 16, 19, 20, 21, 1, 37, 29, 30, 40]
-t3_packages = [5, 9, 2, 4, 7, 8, 10, 11]
-
-
+# creating an array of trucks with their unique packages.
 def load_all_trucks(hashmap):
+    # Create three trucks
+    t1 = Truck()
+    t2 = Truck()
+    t3 = Truck()
+    # Package ids for each Truck
+    t1_packages = [6, 25, 26, 31, 32, 28, 34, 39, 35, 33, 27, 24, 23, 22, 17, 12]
+    t2_packages = [3, 18, 36, 38, 13, 14, 15, 16, 19, 20, 21, 1, 37, 29, 30, 40]
+    t3_packages = [5, 9, 2, 4, 7, 8, 10, 11]
     trucks = []
     truck1 = load_truck(t1, t1_packages, hashmap)
     trucks.append(truck1)
@@ -88,7 +93,7 @@ def load_all_trucks(hashmap):
     trucks.append(truck3)
     return trucks
 
-
+# loading each truck with their package objects.
 def load_truck(truck, truck_packages, pkg_hashmap):
     for p in truck_packages:
         package = pkg_hashmap.look_up(p)
